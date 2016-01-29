04b6831f01e42b250cca398e2c5ea7a4f6525bdd
uppfinnarn <uppfinnarn@gmail.com>
2016-01-29 04:05:29 +0100

    Boost.ProgramOptions for argument parsing

--- include/proxything/app.h
+++ include/proxything/app.h
@@ -1,5 +1,9 @@
+#include <boost/program_options.hpp>
+
 namespace proxything
 {
+	namespace po = boost::program_options;
+	
 	/**
 	 * CLI application frontend for Proxy Thing.
 	 */
@@ -16,13 +20,43 @@ namespace proxything
 		 */
 		virtual ~app();
 		
+		
+		
 		/**
-		 * Main entry point.
+		 * Parses arguments and calls app::run(po::variables_map).
 		 * 
 		 * @param  argc Argument count
 		 * @param  argv Argument values
 		 * @return      Exit code
 		 */
 		int run(int argc, char **argv);
+		
+		
+		
+		/**
+		 * Parses commandline arguments.
+		 * 
+		 * @param  argc Argument count
+		 * @param  argv Argument values
+		 * @return      A parsed map of arguments
+		 */
+		po::variables_map parse_args(int argc, char **argv);
+		
+		/**
+		 * Prints a help message to the user.
+		 */
+		void print_help();
+		
+		/**
+		 * Main entry point.
+		 * 
+		 * @param  args Commandline arguments
+		 * @return      Exit code
+		 */
+		int run(po::variables_map args);
+		
+	protected:
+		/// Option definitions
+		po::options_description cli_options;
 	};
 }
--- src/proxything/app.cpp
+++ src/proxything/app.cpp
@@ -1,15 +1,60 @@
 #include <proxything/app.h>
 #include <iostream>
+#include <string>
 
 using namespace proxything;
 
-app::app() { }
+app::app()
+{
+	cli_options.add_options()
+		("help,?", "print a help message")
+		("host,h", po::value<std::string>()->default_value("127.0.0.1"), "host to bind to")
+		("port,p", po::value<unsigned short>()->default_value(12345), "port to bind to")
+	;
+}
 
 app::~app() { }
 
+
+
 int app::run(int argc, char **argv)
 {
-	std::cout << "Lorem ipsum dolor sit amet" << std::endl;
+	po::variables_map args;
+	
+	try {
+		args = parse_args(argc, argv);
+	} catch(po::error &e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	
+	return run(args);
+}
+
+
+
+po::variables_map app::parse_args(int argc, char **argv)
+{
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, cli_options), vm);
+	po::notify(vm);
+	
+	return vm;
+}
+
+void app::print_help()
+{
+	std::cerr << cli_options;
+}
+
+int app::run(po::variables_map args)
+{
+	if (args.count("help")) {
+		print_help();
+		return 0;
+	}
+	
+	std::cout << "Run on " << args["host"].as<std::string>() << ":" << args["port"].as<unsigned short>() << std::endl;
 	
 	return 0;
 }
