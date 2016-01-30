f0d1b676ea78b7c6bd1336bda58327ea3140379b
uppfinnarn <uppfinnarn@gmail.com>
2016-01-30 22:37:10 +0100

    Commands will never be 1kb long

--- include/proxything/config.h
+++ include/proxything/config.h
@@ -2,7 +2,7 @@
 #define PROXYTHING_CONFIG_H
 
 // Maximum size of the buffer used to read client commands
-#define PROXYTHING_CLIENT_BUFFER_SIZE 1024
+#define PROXYTHING_CLIENT_BUFFER_SIZE 255
 
 // Maximum size of the receive buffer used to read from remote servers
 #define PROXYTHING_REMOTE_BUFFER_SIZE 2048
